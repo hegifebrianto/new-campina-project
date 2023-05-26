db7dc91bab7d8c4c1f5e11ef2c0a78a9a7b6295c
akbar putra <ap975527@gmail.com>
2023-05-26 13:44:25 +0700

    fixing posisi navigasi swiper

--- furbar/assets/js/main.js
+++ furbar/assets/js/main.js
@@ -102,7 +102,6 @@
     var slider = new Swiper('.slider-active .swiper-container', {
         speed: 600,
         effect: "fade",
-        loop: true,
         pagination: {
             el: '.slider-active .swiper-pagination',
             clickable: true,
